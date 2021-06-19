96b151e6a1a0a1f4cadaf336349e5f4db24f3926
dhanushkamath <dhanush2897@gmail.com>
2021-06-19 16:06:56 +0530

    initial commit
    Created starter project, added some basic spring boot dependencies

---
+++ src/main/java/com/dhanushkamath/youtubeapi/YoutubeApiApp.java
@@ -0,0 +1,13 @@
+package com.dhanushkamath.youtubeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeApiApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoutubeApiApp.class, args);
+	}
+
+}
